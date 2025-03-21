a73dc5a95254c16debeaf00109eb224a70b70a21
kwank6704 <nattarat.kwan@gmail.com>
2025-03-21 23:17:58 +0700

    add review promotion

---
+++ src/app/promotion/page.tsx
@@ -0,0 +1,9 @@
+import PromotionSlider from "@/components/PromotionSlider";
+
+export default function Promotion() {
+  return (
+    <main>
+      <PromotionSlider />
+    </main>
+  );
+}
---
+++ src/app/review/page.tsx
@@ -0,0 +1,10 @@
+import ReviewCard from "@/components/ReviewCard";
+import ReviewSlider from "@/components/ReviewSlider";
+
+export default function Review() {
+  return (
+    <main>
+      <ReviewSlider />
+    </main>
+  );
+}
