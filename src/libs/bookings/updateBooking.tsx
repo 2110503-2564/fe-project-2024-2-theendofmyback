10f1b5b30a119a1c8f4b35234c13fa86fc182893
Titiporn Somboon <titiporn02468@gmail.com>
2025-03-22 23:26:15 +0700

    feat: add promotion fetching and selection functionality in booking pages

--- src/libs/bookings/updateBooking.tsx
+++ src/libs/bookings/updateBooking.tsx
@@ -14,6 +14,7 @@ export default async function updateBooking(
         body: JSON.stringify({
             checkInDate: checkInDate,
             checkOutDate: checkOutDate,  
+            ...(promotion ? { promotion } : {}),
         }),
       }
     )
