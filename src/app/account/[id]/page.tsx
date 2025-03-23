7ef6c359695e364bd997bebbcf219668ba9aed62
Titiporn Somboon <titiporn02468@gmail.com>
2025-03-24 04:07:50 +0700

    feat: update README with Vercel link and refactor AccountPage to fetch bookings on session change

--- src/app/account/[id]/page.tsx
+++ src/app/account/[id]/page.tsx
@@ -14,26 +14,7 @@ import PromotionCard from '@/components/PromotionCard';
 import Swal from 'sweetalert2';
 import { Booking } from '../../../../interface';
 
-export default async function AccountPage() {
-
-    const mockBookings = [
-        {
-            _id: "67bf2d872bb021e49823add4",
-            user: "67bf18029a6366536b150330",
-            campground: "67bd6dfcd3e3272696f5243d",
-            checkInDate: "2025-02-26T00:00:00.000+00:00",
-            checkOutDate: "2025-02-27T00:00:00.000+00:00",
-            bookingAt: "2025-02-26T15:04:39.865+00:00"
-        },
-        {
-            _id: "67c02a12fbb0a1e4a90abc22",
-            user: "67c018fa3e6f4a127b2cd560",
-            campground: "67bd6e89a1d3272696f52abc",
-            checkInDate: "2025-03-01T00:00:00.000+00:00",
-            checkOutDate: "2025-03-03T00:00:00.000+00:00",
-            bookingAt: "2025-02-28T12:30:15.420+00:00"
-        },
-    ]
+export default function AccountPage() {
 
     const [name, setName] = useState("");
     const [tel, setTel] = useState("");
@@ -44,7 +25,20 @@ export default async function AccountPage() {
     const [bookingData, setBookingData] = useState<any[]>([])
     const { data: session } = useSession()
 
-    const response: Booking[] = (await getBookings(session?.user?.token || "", "")).data;
+    useEffect(() => {
+        const fetchBookings = async () => {
+            try {
+                const response: Booking[] = (await getBookings(session?.user?.token || "", "")).data;
+                setBookingData(response);
+            } catch (error) {
+                console.error('Error fetching bookings:', error);
+            }
+        };
+
+        if (session?.user?.token) {
+            fetchBookings();
+        }
+    }, [session]);
     
     useEffect(() => {
         const fetchUser = async () => {
