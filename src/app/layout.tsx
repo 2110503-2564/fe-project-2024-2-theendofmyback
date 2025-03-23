7b2db1dc099f9c9a574c68fcd40feb134b00b393
Titiporn Somboon <titiporn02468@gmail.com>
2025-03-24 03:31:18 +0700

    fix suspense

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 import Sidebar, { SidebarItem } from "@/components/Sidebar";
 import { BarChart3, Book, Home, LayoutDashboard, LifeBuoy, Newspaper, Settings, User, UserCircle } from "lucide-react";
 import SessionProviderWrapper from "@/components/SessionProviderWrapper ";
+import { Suspense } from "react";
 
 
 const inter = Inter({ subsets: ["latin"] });
@@ -25,27 +26,27 @@ export default function RootLayout({
         <div className="flex h-screen w-full">
           
           <Sidebar>
-            
-            <SidebarItem icon={<LayoutDashboard size={20} />} text="Home" pageRef = '/'/>
-            <SidebarItem icon={<Home size={20} />} text="Campground" pageRef = '/campground' />
-            <SidebarItem icon={<Book size={20} />} text="Booking" pageRef = '/booking/manage' />
-            <SidebarItem icon={<Newspaper size={20} />} text="Promotion" pageRef = '/promotion' />
-            <SidebarItem icon={<BarChart3 size={20} />} text="Review"  pageRef = '/review'/>
-            
-            <hr className="my-3" />
-            
-            <SidebarItem icon={<UserCircle size={20} />} text="User" pageRef = '/account/1' />
-            <SidebarItem icon={<LifeBuoy size={20} />} text="AboutUs" pageRef = '/about' />
+        
+        <SidebarItem icon={<LayoutDashboard size={20} />} text="Home" pageRef = '/'/>
+        <SidebarItem icon={<Home size={20} />} text="Campground" pageRef = '/campground' />
+        <SidebarItem icon={<Book size={20} />} text="Booking" pageRef = '/booking/manage' />
+        <SidebarItem icon={<Newspaper size={20} />} text="Promotion" pageRef = '/promotion' />
+        <SidebarItem icon={<BarChart3 size={20} />} text="Review"  pageRef = '/review'/>
+        
+        <hr className="my-3" />
+        
+        <SidebarItem icon={<UserCircle size={20} />} text="User" pageRef = '/account/1' />
+        <SidebarItem icon={<LifeBuoy size={20} />} text="AboutUs" pageRef = '/about' />
           
-            
-            
           </Sidebar>
 
           <main className="flex-1 bg-gray-100 overflow-auto ">
-            {children}
+        <Suspense fallback={<div>Loading...</div>}>
+          {children}
+        </Suspense>
           </main>
         </div>
-        </SessionProviderWrapper> 
+      </SessionProviderWrapper> 
       </body>
     </html>
   );
