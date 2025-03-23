dc6ae683951a921e40e70f616119e376d1d01be6
Titiporn Somboon <titiporn02468@gmail.com>
2025-03-23 22:34:11 +0700

    feat: add UploadFile component and update ImageForm to use TypeScript

---
+++ src/app/setting/test/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+import UploadFile from "@/components/Uploadfile";
+
+export default function testadmin(){
+    return(
+        <div>
+            <h1 className="text-gray-400 text-[144px]">อ่านจริงป่าว</h1>
+
+        </div>
+    );
+}
--- src/components/Uploadfile.tsx
+++ src/components/Uploadfile.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ImageForm = () => {
+const ImageForm: React.FC = () => {
   return (
     <StyledWrapper>
       <div className="container"> 
